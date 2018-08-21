4012caf98f1170e922c9bca44aeee25e13e370d0
xiaoran <xiaoran737@163.com>
2018-08-22 00:47:19 +0800

    AtomicInteger原子性操作

--- src/main/java/VolatilePackage/VolatileAtomic.java
+++ src/main/java/VolatilePackage/VolatileAtomic.java
@@ -1,5 +1,7 @@
 package VolatilePackage;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * @author: xiaoran
  * @date: 2018-08-22 00:27
@@ -8,6 +10,8 @@ package VolatilePackage;
  *
  * 若volatile关键字具有原子性，那么结果将会是100000
  * 这里，因为count++是非线程安全的，且volatile不保证原子性，所以在多线程情况下，会导致重复计算
+ *
+ * 采用AtomicInteger类支持原子性操作
  */
 public class VolatileAtomic extends Thread {
     /**
@@ -15,12 +19,20 @@ public class VolatileAtomic extends Thread {
      */
     private static volatile int count = 0;
 
+    /**
+     * 原子性类
+     */
+    private static AtomicInteger atomicCount = new AtomicInteger(0);
+
     @Override
     public void run() {
         for (int i = 0; i < 10000; i++) {
             count++;
+            atomicCount.incrementAndGet();
+            atomicCount.addAndGet(10);
         }
-        System.out.println(count +"  now is:"+System.currentTimeMillis());
+        System.out.println(count +" count now is:"+System.currentTimeMillis());
+        System.out.println(atomicCount +" atomicCount now is:"+System.currentTimeMillis());
     }
 
     public static void main(String[] args) {

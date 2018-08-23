e624eb14eee49cd658017c09f37a575000b9e290
xiaoran <xiaoran737@163.com>
2018-08-23 11:02:30 +0800

    线程之间IO传输    stream方式

--- src/main/java/ThreadPackage/Piped.java
+++ src/main/java/ThreadPackage/Piped.java
@@ -1,6 +1,8 @@
 package ThreadPackage;
 
 import java.io.IOException;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
 import java.io.PipedReader;
 import java.io.PipedWriter;
 
@@ -17,9 +19,13 @@ public class Piped {
         try {
             PipedWriter out = new PipedWriter();
             PipedReader in = new PipedReader();
+
+            PipedOutputStream outputStream = new PipedOutputStream();
+            PipedInputStream inputStream = new PipedInputStream();
             // 将输出流和输⼊流进⾏连接，否则在使⽤时会抛出IOException
             out.connect(in);
-            Thread printThread = new Thread(new PipPrint(in), "PrintThread");
+            outputStream.connect(inputStream);
+            Thread printThread = new Thread(new PipPrint(in,inputStream), "PrintThread");
             printThread.start();
 
             int receive = 0;
@@ -27,6 +33,7 @@ public class Piped {
                 //接收系统输入
                 while ((receive = System.in.read()) != -1) {
                     out.write(receive);
+                    outputStream.write(receive);
                 }
             } finally {
                 out.close();
@@ -42,16 +49,25 @@ public class Piped {
 class PipPrint implements Runnable{
     private PipedReader in;
 
-    public PipPrint(PipedReader pipedReader){
+    private PipedInputStream pipedInputStream;
+
+    public PipPrint(PipedReader pipedReader,PipedInputStream pipedInputStream){
         this.in = pipedReader;
+        this.pipedInputStream = pipedInputStream;
     }
 
     @Override
     public void run() {
         int receive = 0;
+
+        int receiveByStream = 0;
         try{
-            while(((receive = in.read()) != -1)){
-                System.out.println((char) receive);
+//            while(((receive = in.read()) != -1)){
+//                System.out.println((char) receive);
+//            }
+            System.out.println("-------");
+            while(((receiveByStream = pipedInputStream.read()) != -1)){
+                System.out.println((char) receiveByStream);
             }
         }catch (IOException e){
             e.printStackTrace();

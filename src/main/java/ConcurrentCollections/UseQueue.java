828d59274dc5c9c79934035b1d0fe12d7ec6fd6b
xiaoran <xiaoran737@163.com>
2018-09-11 22:49:48 +0800

    三种常用的队列

--- src/main/java/ConcurrentCollections/UseQueue.java
+++ src/main/java/ConcurrentCollections/UseQueue.java
@@ -1,11 +1,36 @@
 package ConcurrentCollections;
 
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 /**
  * @author: xiaoran
  * @date: 2018-08-29 00:14
  */
 public class UseQueue {
+    /**
+     * 非阻塞是队列，采用cas算法来保证   性能很高
+     */
     ConcurrentLinkedQueue<String> queue = new ConcurrentLinkedQueue<String>();
+
+
+
+    /**
+     * 阻塞式队列
+     *
+     * 数组结构的有界阻塞队列
+     */
+    ArrayBlockingQueue<String> arrayBlockingQueue = new ArrayBlockingQueue<String>(16);
+
+    /**
+     * 链表结构的有界阻塞队列
+     *
+     * 默认 2^31 次方
+     */
+    LinkedBlockingQueue<String> linkedBlockingQueue = new LinkedBlockingQueue<>();
+
+
+
+
 }

11f1dd55e27fb0c4d319281be75f0a91c1350d66
xiaoran <xiaoran737@163.com>
2019-01-16 20:12:21 +0800

    future调用方式

--- src/main/java/future/FutureTest.java
+++ src/main/java/future/FutureTest.java
@@ -4,6 +4,8 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 /**
  * future + callable
@@ -13,29 +15,51 @@ import java.util.concurrent.Future;
  */
 public class FutureTest {
     public static void main(String[] args) {
+        /**
+         * 正常返回值的情况
+         */
         ExecutorService executorService = Executors.newCachedThreadPool();
         RunByCallable runByCallable = new RunByCallable();
-        Future<String> result = executorService.submit(runByCallable);
+        Future<String> future = executorService.submit(runByCallable);
         try {
-            System.out.println("callable中call方法执行的结果是：" + result.get());
-            System.out.println("cancel情况： "+result.cancel(true));
-            System.out.println("isCancelled情况： "+result.isCancelled());
-            System.out.println("isDone情况： "+result.isDone());
+            System.out.println("callable中call方法执行的结果是：" + future.get());
+            System.out.println("cancel情况： " + future.cancel(true));
+            System.out.println("isCancelled情况： " + future.isCancelled());
+            System.out.println("isDone情况： " + future.isDone());
         } catch (InterruptedException e) {
             e.printStackTrace();
         } catch (ExecutionException e) {
             e.printStackTrace();
         }
 
+        /**
+         * 抛异常的情况
+         */
         CallableException callableException = new CallableException();
-        Future<String> result2 = executorService.submit(callableException);
+        Future<String> future2 = executorService.submit(callableException);
         try {
-            System.out.println("isDone情况： "+result2.isDone());
-            System.out.println("callable中call方法执行的结果是：" + result2.get());
+            System.out.println("isDone情况： " + future2.isDone());
+            System.out.println("callable中call方法执行的结果是：" + future2.get());
         } catch (InterruptedException e) {
             e.printStackTrace();
         } catch (ExecutionException e) {
             e.printStackTrace();
         }
+
+        /**
+         * 超时
+         */
+        RunByCallable runByCallable3 = new RunByCallable();
+        Future<String> future3 = executorService.submit(runByCallable3);
+        try {
+            System.out.println("callable中call方法执行的结果是：" + future3.get(0, TimeUnit.NANOSECONDS));
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
     }
 }

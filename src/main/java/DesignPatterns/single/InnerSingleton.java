cc09bc95aad97ec0614a028051a5db3deda88a33
xiaoran <xiaoran737@163.com>
2020-04-15 07:45:27 +0800

    静态内部类单例模式

--- src/main/java/DesignPatterns/single/InnerSingleton.java
+++ src/main/java/DesignPatterns/single/InnerSingleton.java
@@ -1,4 +1,4 @@
-package DesignPatterns;
+package DesignPatterns.single;
 
 /**
  * @author: xiaoran
@@ -6,8 +6,14 @@ package DesignPatterns;
  *
  * 静态内部类模式，在多线程场景下，使用最多
  *
+ * 加载InnerSingleton类时，不会立即初始化singleton，只有调用getInstance，才会立即初始化Singleton
+ * 利用jvm加载特性，class文件只会被加载一次，每种class文件都会有一个锁。保证了线程安全和高效(除了第一次加载耗时较长之外)
  */
 public class InnerSingleton {
+
+    private InnerSingleton(){
+    }
+
     private static class Singleton{
         private static Singleton singleton = new Singleton();
     }
@@ -21,5 +27,11 @@ public class InnerSingleton {
         Object o = InnerSingleton.getInstance();
         Object o1 = InnerSingleton.getInstance();
         Object o2 = InnerSingleton.getInstance();
+
+        if(o == o1 && o1== o2){
+            System.out.println(true);
+        }else{
+            System.out.println(false);
+        }
     }
 }
